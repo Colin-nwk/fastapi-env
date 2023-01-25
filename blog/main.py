cb9e7e99ffd854be03f608247c390f94a0c56ad6
Collins Nwoko C <collinsnwoko05@gmail.com>
2023-01-25 10:21:09 +0100

    added sqlalchemy config

--- blog/main.py
+++ blog/main.py
@@ -1,10 +1,12 @@
 from typing import Union
 
 from fastapi import FastAPI
+from blog.schemas import Blog
+
 
 app = FastAPI()
 
 
 @app.post("/blog")
-async def create():
-    return "creating"
+async def create(request: Blog):
+    return request
---
+++ blog/models.py
@@ -0,0 +1,9 @@
+# from pydantic import BaseModel
+# from uuid import UUID, uuid4
+# from typing import Optional, List
+# from enum import Enum
+
+
+# class Blog(BaseModel):
+#     title: str
+#     body: str
