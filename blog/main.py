72fca8dd35702704ffc8ac4ae0d57fb6c6985b40
Collins Nwoko C <collinsnwoko05@gmail.com>
2023-01-25 12:35:47 +0100

    crud

--- blog/main.py
+++ blog/main.py
@@ -32,9 +32,25 @@ async def create(request: schemas.Blog, db: Session = Depends(get_db)):
 @app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
 async def destroy(id, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id ==
-                                        id).delete(synchronize_session=False)
+                                        id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Blog with id: {id} is not available')
+    blog.delete(synchronize_session=False)
     db.commit()
     return "done"
+
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+async def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Blog with id: {id} is not available')
+    blog.update(request)
+    db.commit()
+    return 'updated'
 
 
 @ app.get('/blog')
